b6b8ef4541bcf5911dcb0fe3e59f36a958be0b6a
longpada-zhou <longpanda_zhou@163.com>
2019-10-09 13:42:08 +0800

    dev端口8390改为8190

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./test.env');
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    REQUEST_HOST: '"http://47.99.212.180:8390"',
+    REQUEST_HOST: '"http://47.99.212.180:8190"',
     MAIN_TITLE: '"智造管理平台"',
     LOGIN_TITLE: '"昇虹纺纱智造管理平台"',
     LOGIN_TITLE_MIN: '"纺纱专版"'

b71a5bae8622a95171a081b6d5de76e9eb94f9e8
longpada-zhou <longpanda_zhou@163.com>
2019-10-29 12:02:37 +0800

    modify prop 8390 port

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"production"',
-    REQUEST_HOST: '"http://47.99.212.180:8191"'
+    REQUEST_HOST: '"http://47.99.212.180:8390"'
     // REQUEST_HOST: '"https://service.shengtex.com"'
 };
